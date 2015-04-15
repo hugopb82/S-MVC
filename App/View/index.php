6b092c479eb1307a3e8171cfec337d147405ec93
hugopb82 <cedric@lubuntu>
2015-04-15 16:57:35 +0200

    New changes

--- App/View/index.php
+++ App/View/index.php
@@ -1,8 +1,2 @@
 <p>This is the index page</p>
 <?= isset($message) ? $message : '' ?>
-<?php 
-	$form = new Form('');
-	$form->add('text', 'title', null);
-    $form->add('password', 'password');
-     echo($form->end());
-?>
