585113fd5f64da53eddd6980e92200426702dcf7
hugopb82 <=>
2015-06-30 21:06:10 +0200

    Working on Upstaer

--- Kernel/Updater.php
+++ Kernel/Updater.php
@@ -15,8 +15,22 @@
         }
 
         public function check(){
-            $last_json = file_get_contents('https://www.github');
-            $last_version = file_get_contents();
+            $last_json = file_get_contents('https://raw.githubusercontent.com/hugopb82/S-MVC/master/Kernel/updater_info.json');
+            $last_info = json_decode($last_json);
+            $last_version = $last_info->version;
+            if($this->version != $last_version){
+                file_put_contents(ROOT . 'latest.zip', fopen("https://github.com/hugopb82/S-MVC/archive/master.zip", 'r'));
+
+                $zip = new \ZipArchive;
+                $res = $zip->open(ROOT . 'latest.zip');
+                if($res === TRUE){
+                    $zip->extractTo(ROOT . 'tmp');
+                    $zip->close();
+                    unlink(ROOT . 'latest.zip');
+                    rmdir(ROOT . 'Kernel');
+                    rename(ROOT . 'tmp/S-MVC-master/Kernel', ROOT . 'Kernel');
+                }
+            }
         }
 
     }
